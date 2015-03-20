5a3d46d4c22ad41a5006b4730714fc3b1e511d9a
mihirkelkar <kelkarmhr@gmail.com>
2015-03-20 13:55:00 -0400

    Minor Improvement for the controller

--- js/script.js
+++ js/script.js
@@ -5,9 +5,15 @@ angular.module( "tldr", ['ngAutocomplete'])
     $scope.resultsentence="";
     $scope.submit = function() {
         //console.log($scope.resultserver);
-        $http.get("weather/" + $scope.resultserver)
+        $scope.dataarray = $scope.resultserver.split(",");
+        if($scope.dataarray.length <= 2){
+          $scope.data = $scope.dataarray.join("");
+        } else {
+          $scope.data = $scope.dataarray.slice(0, 2).join("");
+        }
+        $http.get("weather/" + $scope.data)
         .success(function(response) {
-            console.log(response);
+            //console.log(response);
             $scope.resultimage = response.image;
             $scope.resultsentence = response.sentence;
             

09372b960a2b06b377da5cad68752a654bdb9a0f
mihirkelkar <kelkarmhr@gmail.com>
2015-03-19 23:07:03 -0400

    Deploying angular app with get requests

--- Client/script.js
+++ Client/script.js
@@ -1,7 +1,12 @@
 angular.module( "tldr", ['ngAutocomplete'])
-  .controller("tldrCtrl",function ($scope) {
-
+  .controller("tldrCtrl",function ($scope, $http) {
     $scope.resultserver = '';
     $scope.resultimage= "";
     $scope.resultsentence="";
+
+    $http.get("http://tldr-weather.herokuapp.com/weather/" + $scope.resultserver)
+    .success(function(response) {
+    	$scope.resultimage = response.image;
+    	$scope.resultsentence = response.sentence;
+    });
 });
